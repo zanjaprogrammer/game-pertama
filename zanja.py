e15572a4d44eaee0c7344d7bbcbb391b048d30a8
zanjaprogrammer <33482311+zanjaprogrammer@users.noreply.github.com>
2017-11-10 13:57:16 +0700

    ke atas sudah bisa dan turun

--- zanja.py
+++ zanja.py
@@ -7,6 +7,7 @@ player = pygame.sprite.Sprite()
 player.image = pygame.image.load('playerkiri.png')
 player.rect = player.image.get_rect()
 kiri = 200
+atas = 250
 player.rect.topleft = [kiri, 250]
 screen.blit(player.image, player.rect)
 clock = pygame.time.Clock()
@@ -25,37 +26,21 @@ while power > 0:
 
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_LEFT]:
-        print('kiri')
         power = power - 1
-        print(power)
         kiri = kiri - 10
-        player.rect.topleft = [kiri, 250]
-        screen.fill((0, 0, 0))
-        screen.blit(player.image, player.rect)
+
     if pressed[pygame.K_RIGHT]:
-        print('kanan')
         power = power + 1
-        print(power)
         kiri = kiri + 10
-        player.rect.topleft = [kiri, 250]
-        screen.fill((0, 0, 0))  # fill the screen
-        screen.blit(player.image, player.rect)
-    # if keyboard.is_pressed('a'):
-    #     print('kiri')
-    #     power = power - 0.01
-    #     print(power)
-    # if keyboard.is_pressed('d'):
-    #     print('kanan')
-    #     power = power + 0.01
-    #     print(power)
-    # if keyboard.is_pressed('w'):
-    #     print('maju')
-    # if keyboard.is_pressed('s'):
-    #     print('mundur')
-    #
-    # if keyboard.is_pressed('esc'):
-    #     break
-    #
+
+    if pressed[pygame.K_UP]:
+        atas = atas - 10
+
+    if pressed[pygame.K_DOWN]:
+        atas = atas + 10
+    player.rect.topleft = [kiri, atas]
+    screen.fill((0, 0, 0))
+    screen.blit(player.image, player.rect)
 
     pygame.display.flip()
     clock.tick(40)
