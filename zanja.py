ab540154086f3e3b2712c7b190c09d93768e4769
zanjaprogrammer <33482311+zanjaprogrammer@users.noreply.github.com>
2017-11-17 14:24:11 +0700

    musuhnya sudah bisa di gerakkan

--- zanja.py
+++ zanja.py
@@ -66,7 +66,17 @@ while power > 0:
     if pressed[pygame.K_DOWN] or v2 == 0.999969482421875:
         atas_player = atas_player + 10
 
+    if h1 == -1:
+        kiri_enemy = kiri_enemy - 10
+    if h1 == 0.999969482421875:
+        kiri_enemy = kiri_enemy + 10
+    if v1 == -1:
+        atas_enemy = atas_enemy - 10
+    if v1 == 0.999969482421875:
+        atas_enemy = atas_enemy + 10
+
     player.rect.topleft = [kiri_player, atas_player]
+    enemy.rect.topleft = [kiri_enemy, atas_enemy]
     screen.fill((255,0 ,0 ))
     screen.blit(player.image, player.rect)
     screen.blit(enemy.image, enemy.rect)
